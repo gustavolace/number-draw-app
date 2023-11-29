cc6b884a87dc98de0593bcd1e104e7d21af50a81
gustavolace <lsbgustavo@gmail.com>
2023-11-29 14:56:10 -0300

    text set

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarBotao(View v) {
+        TextView texto = findViewById(R.id.resultText);
+        texto.setText("TEXTO ALTERADO");
     }
 }
